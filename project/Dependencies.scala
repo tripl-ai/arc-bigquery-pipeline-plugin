68b3071cdb56c1e67154c45a6b61edd4e4e21d46
Mike Seddon <seddonm1@gmail.com>
2020-09-07 11:11:07 +1000

    simplify dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,6 @@ object Dependencies {
   // spark
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
 
-  val googleBigQuery = "com.google.cloud" % "google-cloud-bigquery" % "1.116.8"
   val sparkBigQuery = "com.google.cloud.spark" %% "spark-bigquery-with-dependencies" % "0.17.1"
 
   // Project
@@ -22,7 +21,6 @@ object Dependencies {
     scalaTest,
     arc,
     sparkSql,
-    googleBigQuery,
     sparkBigQuery
   )
 }
